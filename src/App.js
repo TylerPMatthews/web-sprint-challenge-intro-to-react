65abddaf274b45620774f285aeff2b4b10d90e6e
ty <tylerpmatthews1998@gmail.com>
2020-10-09 15:46:28 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Axios from "axios";
-import {Character} from './components/Character.js'
+import  Character  from "./components/Character.js";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,7 +10,8 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [charactersData, setCharactersData] = useState(true);
+  const [charactersData, setCharactersData] = useState([]);
+  // const data = useState([])
   useEffect(() => {
     Axios.get("http://swapi.dev/api/people/")
       .then((res) => {
@@ -21,11 +22,13 @@ const App = () => {
       });
   }, []);
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-      <Character data={charactersData}/>
-    </div>
+    <>
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        {charactersData.map((character, index) => (
+        <Character key={index} character={character} />))}
+      </div>
+    </>
   );
 };
-
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-export  function Character(props){
-
-   
-
-
-    return (
-        <>
-            <h1>People</h1>
-            {props.data.results.map((item,i)=>{
-                debugger;
-                return(
-                    <div id={i}>
-                    <h1>{item.name}</h1>
-                    </div>
-                    
-                )
-            })}
-
-        </>
-    )
+export default function Character(data) {
+  return (
+    <div className="App">
+      <h1>Name: {data.character.name}</h1>
+      <img src={data.character.url} alt={data.character.alt} />
+      <h3>Created: {data.character.created}</h3>
+      <h3>Edited: {data.character.edited}</h3>
+      <p>height: {data.character.height}</p>
+      <p>Hair Color: {data.character.mass}</p>
+      <div>
+        <p>Vehicles: {data.character.vehicles}</p>
+        <p>Starships: {data.character.starships}</p>
+      </div>
+    </div>
+  );
 }
