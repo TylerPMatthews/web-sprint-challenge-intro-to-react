9b826102fa8600e2afe71a1612535f1b4da850bd
ty <tylerpmatthews1998@gmail.com>
2020-10-09 13:17:14 -0400

    Bugs in my map method. Other than that really close.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import {Character} from './components/Character.js'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [charactersData, setCharactersData] = useState(true);
+  useEffect(() => {
+    Axios.get("http://swapi.dev/api/people/")
+      .then((res) => {
+        setCharactersData(res.data.results);
+      })
+      .catch((err) => {
+        console.log("error has occured");
+      });
+  }, []);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character data={charactersData}/>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+export  function Character(props){
+
+   
+
+
+    return (
+        <>
+            <h1>People</h1>
+            {props.data.results.map((item,i)=>{
+                debugger;
+                return(
+                    <div id={i}>
+                    <h1>{item.name}</h1>
+                    </div>
+                    
+                )
+            })}
+
+        </>
+    )
+}
