1516f90f89a6be8a8020223c9705a4052762a5e3
ty <tylerpmatthews1998@gmail.com>
2020-11-06 12:37:38 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import Character from "./components/Character.js";
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [charactersData, setCharactersData] = useState([]);
+  useEffect(() => {
+    Axios.get("https://rickandmortyapi.com/api/character/")
+      .then((res) => {
+        setCharactersData(res.data.results);
+      })
+      .catch((err) => {
+        console.log("error has occured");
+      });
+  }, []);
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        {charactersData.map((character, index) => (
+          <Character key={index} character={character} />
+        ))}
+      </div>
+    </>
   );
-}
-
+};
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,52 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+  border: 3px solid black;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  margin-right:10%;
+  margin-left:10%;
+  background-color: white;
+  color: black;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  text-align: center;
+
+  &:hover {
+    background-color: black;
+    color:white;
+    border: 3px solid white;
+    transform: scale(1.2);
+    transition:transform 0.5s;
+
+    transition all 0.5s ease-in-out;
+  }
+  transition all 0.5s ease-in-out;
+  transform: scale(1);
+  transition:transform 0.7s;
+
+
+  h1 {
+    color: red;
+  }
+  span{
+   color:blue;
+  }
+ 
+`;
+
+export default function Character(props) {
+  return (
+    <StyledCharacter className="App">
+      <img src={props.character.image} />
+      <h1>Name: {props.character.name}</h1>
+      <p>
+        <span>{props.character.status}</span>
+      </p>
+      <p>Species: {props.character.species}</p>
+      <p>Gender: {props.character.gender}</p>
+      <p>Created: {props.character.created}</p>
+    </StyledCharacter>
+  );
+}
